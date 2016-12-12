d72f105a4cd0333cd5ee79c13cd6b3b8a94fcb3c
Nick Taft <taft82@gmail.com>
2016-12-11 20:17:33 -0500

    got all of the song data to properly save in db

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,11 +1,11 @@
-const pgp = require('pg-promise');
+const pgp = require('pg-promise')({});
 
 const config = {
-  host:     process.env.PG_HOST || 'ec2-107-20-198-81.compute-1.amazonaws.com',
+  host:     process.env.PG_HOST || 'localhost',
   port:     process.env.PG_PORT || 5432,
-  database: process.env.PG_DATABASE || 'd3lv70inldpqnh',
-  user:     process.env.PG_USER || 'qizuytuxsoyqno',
-  password: process.env.PG_PASSWORD || 'hOttbp99DvvW1KJZanY4mgnsSd',
+  database: process.env.PG_DATABASE || 'tappity',
+  // user:     process.env.PG_USER || 'qizuytuxsoyqno',
+  // password: process.env.PG_PASSWORD || 'hOttbp99DvvW1KJZanY4mgnsSd',
 };
 
 module.exports = pgp(config);
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
@@ -10,16 +10,16 @@ CREATE TABLE users (
   accessedDate TIMESTAMP
 );
 
-DROP TABLE IF EXISTS tracks;
+DROP TABLE IF EXISTS tracks CASCADE;
 
 CREATE TABLE tracks (
   id SERIAL PRIMARY KEY,
   user_id INT REFERENCES users(id),
   track_name VARCHAR(80),
-  instruments VARCHAR(255)[]
+  instruments VARCHAR(510)[]
 );
 
-DROP TABLE IF EXISTS sounds;
+DROP TABLE IF EXISTS sounds CASCADE;
 
 CREATE TABLE sounds (
   id SERIAL PRIMARY KEY,
@@ -28,13 +28,13 @@ CREATE TABLE sounds (
   file_path VARCHAR(255)
 );
 
-DROP TABLE IF EXISTS track_data;
+DROP TABLE IF EXISTS track_data CASCADE;
 
 CREATE TABLE track_data (
   track_id INT REFERENCES tracks(id),
-  sound_id INT REFERENCES sounds(id),
-  beat_id INT,
-  beat_time INT
+  sound_name VARCHAR(80),
+  beat_id SMALLINT,
+  beat_time BIGINT
 );
 
 COMMIT;
