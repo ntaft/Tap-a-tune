1b4e7ae4f0eabbbdd050c62cd2214dc12acf9a6e
Nick Taft <taft82@gmail.com>
2017-01-04 21:12:53 -0500

    minor tweaks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import './Header.css'
 
 const Header = props => (
-  <div
-    className="header"
-  ><div className="logo-box">
+  <div className="header">
+    <div className="logo-box">
 
-    <img
-    src="http://freevector.co/wp-content/uploads/2013/05/56093-triple-tap-of-three-fingers-of-a-hand-outlined-gesture-symbol-200x200.png"
-    className="logo"
-  />
-  </div>
+      <img
+      src="http://freevector.co/wp-content/uploads/2013/05/56093-triple-tap-of-three-fingers-of-a-hand-outlined-gesture-symbol-200x200.png"
+      className="logo"
+    />
+    </div>
     <h1>Tappity</h1>
     <div className={props.userId === 0 ? "login-container" : "hide login-container"}>
       <div
